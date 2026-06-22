1818b965a36602fed83e4b684f4ce38e62e23120
Faisal Meeran <memomezan30@gmail.com>
2026-06-22 16:29:27 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-$server = "warranty-sql-server-elliott.database.windows.net";
-$database = "warranty-sql-server-elliott";
+$server = "warrantyportaldb.database.windows.net";
+$database = "warrantyportaldatabase";
 $username = "fmtest";
 $password = "sqladminuser@123";
 
